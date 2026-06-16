5d7b762d930c32c532ef76948f015e7cf473abcd
Hxzithx <hasithalakruwan081@gmail.com>
2026-06-17 03:32:16 +0530

    Add comment entity

--- src/main/java/com/example/blog_app/entity/Comment.java
+++ src/main/java/com/example/blog_app/entity/Comment.java
@@ -1,5 +1,27 @@
 package com.example.blog_app.entity;
 
-public class Comment {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
 
+@Data
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
