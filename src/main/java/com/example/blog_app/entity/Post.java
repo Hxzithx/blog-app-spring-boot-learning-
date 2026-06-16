13a37df0aeaad9a5d6b12a71794ca6377b97e11a
Hxzithx <hasithalakruwan081@gmail.com>
2026-06-17 03:21:16 +0530

    Add post entity

--- src/main/java/com/example/blog_app/entity/Post.java
+++ src/main/java/com/example/blog_app/entity/Post.java
@@ -1,4 +1,31 @@
 package com.example.blog_app.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 5000)
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments;
 }
