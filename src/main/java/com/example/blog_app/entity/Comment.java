e0941349c0ec33929daf75445b67775ea8b07e6c
Hxzithx <hasithalakruwan081@gmail.com>
2026-06-17 02:13:15 +0530

    initial commit

---
+++ src/main/java/com/example/blog_app/entity/Comment.java
@@ -0,0 +1,5 @@
+package com.example.blog_app.entity;
+
+public class Comment {
+
+}
---
+++ src/main/java/com/example/blog_app/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.blog_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
